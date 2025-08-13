4af3083062559cbe10b4846edcab10cd76e1fb94
ababab820113 <ababab820113@gmail.com>
2025-08-13 12:53:40 +0800

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@
 function bcalcBmi() {     //為了跟按鈕綁在一起的function
 	let vhh = document.getElementById('thh').value; //把物件(id))抓出來,id不能重複
 	let vww = document.getElementById('tww').value;
-	let bmi = calcBmi(vww,vhh);
+	let bmi = calcBmi(vww, vhh);
 	let category = getBmiCategory(bmi); // 取得分類
 	document.getElementById('rBmi').textContent = `BMI值為: ${bmi} (${category})`;
 }
@@ -21,11 +21,11 @@ function calcBmi(w, h) {
 
 // BMI 分類
 function getBmiCategory(bmi) {
-    bmi = parseFloat(bmi);
-    if (bmi < 18.5) return "過輕";
-    if (bmi < 24) return "正常";
-    if (bmi < 27) return "過重";
-    return "肥胖"; 
+	bmi = parseFloat(bmi);
+	if (bmi < 18.5) return "過輕";
+	if (bmi < 24) return "正常";
+	if (bmi < 27) return "過重";
+	return "肥胖";
 }
 
 
@@ -33,16 +33,16 @@ function getBmiCategory(bmi) {
 
 //把計算的function呼叫出來的function
 
-function bC2F(){
-let vcc = document.getElementById('tcc').value;
-let vff = c2f(vcc)
-document.getElementById('tff').value = vff
+function bC2F() {
+	let vcc = document.getElementById('tcc').value;
+	let vff = c2f(vcc)
+	document.getElementById('tff').value = vff
 }
 
-function bF2C(){
-let vff = document.getElementById('tff').value;
-let vcc = f2c(vff)
-document.getElementById('tcc').value = vcc
+function bF2C() {
+	let vff = document.getElementById('tff').value;
+	let vcc = f2c(vff)
+	document.getElementById('tcc').value = vcc
 
 }
 
@@ -62,10 +62,10 @@ function f2c(f) {
 
 // 是否閏年******************************************
 //把計算的function呼叫出來的function
-function isLeapYear1(){
+function isLeapYear1() {
 	let vyy = document.getElementById('tyy').value;
 	let isLeap = isLeapYear2(vyy);
-	let s = isLeap ? '是' : '不是' ;
+	let s = isLeap ? '是' : '不是';
 	document.getElementById('rIsLeap').textContent = `${vyy}年 ${s}閏年`;
 }
 
@@ -81,14 +81,14 @@ isLeapYear2(2025);
 
 // 房貸******************************************
 
-function bEpp(){
-		let vpp = document.getElementById('tpp').value;
-		let vrr = document.getElementById('trr').value;
-		let vnn = document.getElementById('tnn').value;
+function bEpp() {
+	let vpp = document.getElementById('tpp').value * 10000;
+	let vrr = document.getElementById('trr').value /  100;
+	let vnn = document.getElementById('tnn').value;
 
-		let epp = funEPP(vpp, vrr, vnn);
+	let epp = funEPP(vpp, vrr, vnn);
 
-		document.getElementById('rEpp').textContent = `每月應付本利和 ${epp}元`;
+	document.getElementById('rEpp').textContent = `每月應付本利和 ${epp}元`;
 }
 
 
@@ -106,24 +106,24 @@ function funEPP(p, r, ny) {
 
 //*************************************************************
 
-let weeks= ['星期日','星期一','星期二','星期三','星期四','星期五','星期六'];
+let weeks = ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六'];
 
 function getTimeStr() {
-  let now = new Date();                // 取得現在時間
-  let y = now.getFullYear();          // 年
-  let M = now.getMonth() + 1;         // 月（從0開始，要+1）
-  let d = now.getDate();              // 日
-  let h = String(now.getHours()).padStart(2, '0');    // 小時（補0）
-  let m = String(now.getMinutes()).padStart(2, '0');  // 分（補0）
-  let s = String(now.getSeconds()).padStart(2, '0');  // 秒（補0）
-  let w = weeks[now.getDay()];        // 星期（日=0, 一=1...）
-  let str = `${y}/${M}/${d} ${w} ${h}:${m}:${s}`;      // 組合成時間字串
-  return str;
+	let now = new Date();                // 取得現在時間
+	let y = now.getFullYear();          // 年
+	let M = now.getMonth() + 1;         // 月（從0開始，要+1）
+	let d = now.getDate();              // 日
+	let h = String(now.getHours()).padStart(2, '0');    // 小時（補0）
+	let m = String(now.getMinutes()).padStart(2, '0');  // 分（補0）
+	let s = String(now.getSeconds()).padStart(2, '0');  // 秒（補0）
+	let w = weeks[now.getDay()];        // 星期（日=0, 一=1...）
+	let str = `${y}/${M}/${d} ${w} ${h}:${m}:${s}`;      // 組合成時間字串
+	return str;
 }
 
 //秀出時間
 function showStr(str) {
-  document.getElementById('tt').textContent = str;
+	document.getElementById('tt').textContent = str;
 }
 
 //直接執行，但有小問題:如果程式碼很大，畫面上的HYML還沒下載完畢，可能會有錯誤
@@ -134,7 +134,7 @@ function showStr(str) {
 //現在要開始讓秒數動起來(把showTime(getTimeStr());包起來)
 
 function showTimeStr() {
-  showStr(getTimeStr());
+	showStr(getTimeStr());
 }
 
 showTimeStr();
@@ -146,19 +146,20 @@ showStr('請開始');
 
 //一樣用function套起來
 let sid = null; //讓Start做一個保護措施，重新按開始按鈕就不會有bug
-function start(){
-  if (sid){
-    return;
-  }
-  sid = setInterval(showTimeStr,1000);
-  showTimeStr();
+function start() {
+	if (sid) {
+		return;
+	}
+	sid = setInterval(showTimeStr, 1000);
+	showTimeStr();
 }
 
-function stop(){
-  clearInterval(sid);
-  sid = null 
-  showStr('請開始');
+function stop() {
+	clearInterval(sid);
+	sid = null
+	showStr('請開始');
 }
+
 
 
 
